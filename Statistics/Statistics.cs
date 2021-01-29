5a254cd9534aa96aa24c30ec0188a646fa36ed8e
savitatarakar <77490717+savitatarakar@users.noreply.github.com>
2021-01-29 20:31:41 +0530

    computing statistics

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -6,7 +6,12 @@ namespace Statistics
     public class StatsComputer
     {
         public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+            Stats stats = new Stats();
+            stats.average = numbers.ToArray().Average();
+            stats.max = numbers.ToArray().Max();
+            stats.min = numbers.ToArray().Min();
+
+            return stats;
         }
     }
 }
